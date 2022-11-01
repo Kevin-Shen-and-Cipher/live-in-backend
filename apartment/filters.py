2a1aa3f472bae93b3094b563ab1c3eb144e6e23d
MoonNight <k0937477331@gmail.com>
2022-11-01 18:49:37 +0800

    fix:Fix wrong variable type

--- apartment/filters.py
+++ apartment/filters.py
@@ -18,8 +18,11 @@ class ApartmentFilter(object):
     # 區域篩選
     def filter_district(querys: QueryDict, queryset: QuerySet):
         district = querys.getlist("district")
+
         if (district):
-            queryset = queryset.filter(distrcit__in=district)
+            district = list(map(int, district))
+
+            queryset = queryset.filter(district__in=district)
 
         return queryset
 
@@ -29,15 +32,21 @@ class ApartmentFilter(object):
         max_price = querys.get("max_price")
 
         if (min_price and max_price):
+            min_price = int(min_price)
+            max_price = int(max_price)
+
             queryset = queryset.filter(
-                price__range=(int(min_price), int(max_price)))
+                price__range=(min_price, max_price))
 
         return queryset
 
     # 租屋類型篩選
     def filter_rent_type(querys: QueryDict, queryset: QuerySet):
         rent_types = querys.getlist("rent_type")
+        
         if (rent_types):
+            rent_types = list(map(int, rent_types))
+
             queryset = queryset.filter(rent_type__in=rent_types)
 
         return queryset
@@ -45,7 +54,10 @@ class ApartmentFilter(object):
     # 房子類型篩選
     def filter_apartment_type(querys: QueryDict, queryset: QuerySet):
         apartment_types = querys.getlist("apartment_type")
+
         if (apartment_types):
+            apartment_types = list(map(int, apartment_types))
+
             queryset = queryset.filter(apartment_type__in=apartment_types)
 
         return queryset
@@ -53,7 +65,10 @@ class ApartmentFilter(object):
     # 格局篩選
     def filter_room_type(querys: QueryDict, queryset: QuerySet):
         room_types = querys.getlist("room_type")
+
         if (room_types):
+            room_types = list(map(int, room_types))
+
             queryset = queryset.filter(room_type__in=room_types)
 
         return queryset
@@ -61,7 +76,10 @@ class ApartmentFilter(object):
     # 限制篩選
     def filter_restrict(querys: QueryDict, queryset: QuerySet):
         restricts = querys.getlist("restrict")
+
         if (restricts):
+            restricts = list(map(int, restricts))
+
             queryset = queryset.filter(restrict__in=restricts)
 
         return queryset
@@ -69,7 +87,10 @@ class ApartmentFilter(object):
     # 設備篩選
     def filter_device(querys: QueryDict, queryset: QuerySet):
         devices = querys.getlist("device")
+
         if (devices):
+            devices = list(map(int, devices))
+
             queryset = queryset.filter(device__in=devices)
 
         return queryset
--- job/filters.py
+++ job/filters.py
@@ -15,8 +15,11 @@ class JobFilter(object):
     # 區域篩選
     def filter_district(querys: QueryDict, queryset: QuerySet):
         district = querys.getlist("district")
+
         if (district):
-            queryset = queryset.filter(distrcit__in=district)
+            district = list(map(int, district))
+
+            queryset = queryset.filter(district__in=district)
 
         return queryset
 
@@ -25,15 +28,20 @@ class JobFilter(object):
         min_salary = querys.get("min_salary")
 
         if (min_salary):
+            min_salary = int(min_salary)
+
             queryset = queryset.filter(
-                price__gte=int(min_salary))
+                salary__gte=min_salary)
 
         return queryset
 
     # 職位篩選
     def filter_job_position(querys: QueryDict, queryset: QuerySet):
         job_positions = querys.getlist("job_position")
+        
         if (job_positions):
+            job_positions = list(map(int, job_positions))
+
             queryset = queryset.filter(job_position__in=job_positions)
 
         return queryset
@@ -41,7 +49,10 @@ class JobFilter(object):
     # 工作時段篩選
     def filter_working_hour(querys: QueryDict, queryset: QuerySet):
         working_hours = querys.getlist("working_hour")
+
         if (working_hours):
+            working_hours = list(map(int, working_hours))
+
             queryset = queryset.filter(working_hour__in=working_hours)
 
         return queryset
